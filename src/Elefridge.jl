acf3657e6424430c284aa37ad992a20aad7826ae
milankl <milankloewer@gmx.de>
2020-04-19 16:54:16 +0100

    pkg template

---
+++ src/Elefridge.jl
@@ -0,0 +1,5 @@
+module Elefridge
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Elefridge
+using Test
+
+@testset "Elefridge.jl" begin
+    @test true     # to be extended
+end
