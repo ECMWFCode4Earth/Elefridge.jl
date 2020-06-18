9167c894c78c1ed813d216e41c8e3c248b72d165
Milan K <milankloewer@gmx.de>
2020-06-18 22:54:03 +0100

    laura made the plot beautiful

--- wip/error_size_comparison.jl
+++ wip/error_size_comparison.jl
@@ -125,40 +125,56 @@ Xcc = compress(X,level=9)
 bloscc = sizeof(X)/sizeof(Xcc)
 
 ## theoretical
-theosize = [32/i for i in 32:-1:10]
+theosize = [32/i for i in 32:-1:5]
 x0 = 1f0
-theoerror = [log10(nextfloat(x0,2^i)) for i in 0:22]
+theoerror = [log10(nextfloat(x0,2^i)) for i in 0:27]
 
 ## plotting
 pygui(true)
 n = size(EV)[1]
-colours = ["C$i" for i in 0:n-1]
+
+cm1 = ColorMap("viridis")
+cm2 = ColorMap("inferno")
+
+function RGB(cv::Tuple)
+    r = UInt8(round(cv[1]*255))
+    g = UInt8(round(cv[2]*255))
+    b = UInt8(round(cv[3]*255))
+    return "#"*repr(r)[3:4]*repr(g)[3:4]*repr(b)[3:4]
+end
+
+colours = ["0.5","0.3",RGB(cm2(0.9)),RGB(cm2(0.8)),RGB(cm2(0.6)),RGB(cm2(0.4)),
+                    RGB(cm1(0.8)),RGB(cm1(0.6)),RGB(cm1(0.4)),RGB(cm1(0.2))]
+
 names = ["LinQuant24","LogQuant16","Bitgroom16+Blosc","Bitgroom18+Blosc",
             "Bitgroom20+Blosc","Bitgroom24+Blosc","RoundNearest16+Blosc",
             "RoundNearest18+Blosc","RoundNearest20+Blosc","RoundNearest24+Blosc"]
 
+markers = ["s","s","d","d","d","d","o","o","o","o"]
+
 fig,ax = subplots(1,1,figsize=(8,6))
 ax.set_xscale("log")
 
-for i in 1:n
-    ax.plot(EV[i,3:4],[EV[i,1],EV[i,1]],colours[i],lw=3,zorder=-1,label=names[i])
-    ax.plot(EV[i,4:5],[EV[i,1],EV[i,1]],colours[i],lw=1.5,zorder=-1)
+ax.plot(theoerror,theosize,"k")
+ax.plot(eps(1f0)/2e0,1,"w",markersize=15,marker="*",lw=0,markeredgewidth=1.4,
+            markeredgecolor="k",alpha=1,label="Float32",zorder=10)
+ax.plot(eps(1f0)/2e0,bloscc,"w",markersize=10,marker="v",lw=0,markeredgewidth=1.4,
+            markeredgecolor="k",alpha=0.7,label="Float32+Blosc",zorder=10)
+
+for i in [1,2,7,8,9,10,3,4,5,6]
+    # ax.plot(EV[i,[3,5]],[EV[i,1],EV[i,1]],"k",lw=5.5,zorder=-1)
+    ax.plot(EV[i,3:4],[EV[i,1],EV[i,1]],colours[i],
+            marker=markers[i],markevery=2,
+            markeredgecolor="k",markersize=10,
+            lw=5,label=names[i])
+    ax.plot(EV[i,4:5],[EV[i,1],EV[i,1]],colours[i],lw=2)
 end
 
-ax.scatter(EV[:,2],EV[:,1],50,colours,marker="d",edgecolor="k")
-ax.scatter(EV[:,3],EV[:,1],50,colours,marker="o",edgecolor="k")
-
-ax.scatter(eps(1f0)/2e0,1,140,"#00FF00",marker="*",
-            edgecolor="k",alpha=0.7,label="Float32",zorder=10)
-ax.scatter(eps(1f0)/2e0,bloscc,140,"#00FF00",marker="s",
-            edgecolor="k",alpha=0.7,label="Float32+Blosc",zorder=10)
-ax.plot(theoerror,theosize,"k")
-
-
 ax.set_xlim(eps(1f0)/3,1)
-ax.set_ylim(0.9,3.5)
+ax.set_ylim(0.6,3.5)
 ax.set_xlabel("decimal error")
 ax.set_ylabel("compression factor")
-ax.legend(loc=1)
+ax.legend(loc=4,ncol=3,fontsize=9)
+ax.set_title(L"NO$_2$ compression",loc="left")
 
 tight_layout()
