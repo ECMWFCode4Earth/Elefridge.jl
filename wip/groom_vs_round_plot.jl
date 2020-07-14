765cfa345243890ef6f1c2aeddab19ed6286b062
Milan K <milankloewer@gmx.de>
2020-07-14 19:33:33 +0100

    mean error with bit shaving

--- wip/groom_vs_round_plot.jl
+++ wip/groom_vs_round_plot.jl
@@ -4,11 +4,12 @@ using PyPlot
 using StatsBase
 
 N = 10000
-m = 100000
+m = 10000
 q = 7
 
 errorsr = fill(0f0,q,N)
 errorsg = fill(0f0,q,N)
+errorss = fill(0f0,q,N)
 
 for i in 1:N
     # sample various distributions
@@ -21,6 +22,7 @@ for i in 1:N
                             exp.(randn(Float32,m))])
         errorsr[s,i] = mean(D - round(D,7))
         errorsg[s,i] = mean(D - groom(D,7))
+        errorss[s,i] = mean(D - shave(D,7))
     end
  end
 
@@ -33,16 +35,17 @@ lwmm = 1
 lw10 = 3
 lw25 = 8
 
-distr = [   "Uniform(0,1)","Uniform(1,2)","Uniform(0,4)",
-            "Normal(0,1)","Normal(1,1)","Normal(0,4)",
+distr = [   "Uniform(0,1)","Uniform(0,4)","Uniform(1,2)",
+            "Normal(0,1)","Normal(0,4)","Normal(1,1)",
             "Lognormal(0,1)"]
 
 for i in 1:q
     eround = abs.(errorsr[i,:])
     egroom = abs.(errorsg[i,:])
+    eshave = abs.(errorss[i,:])
 
-    for (s,e) in enumerate([eround,egroom])
-        y = i-s/3
+    for (s,e) in enumerate([eround,egroom,eshave])
+        y = i-s/4
         c = "C"*string(s-1)
         ax.semilogx([minimum(e),maximum(e)],[y,y],c,lw=lwmm)
         p10 = percentile(e,10)
@@ -53,13 +56,16 @@ for i in 1:q
         ax.semilogx([p10,p90],[y,y],c,lw=lw10)
         ax.semilogx([p25,p75],[y,y],c,lw=lw25)
         ax.scatter(p50,y,100,c,edgecolor="k",zorder=5)
+        ax.axhline(i,color="k",lw=0.5)
     end
-    ax.text(1.5e-4,i-1/2,distr[i],va="center")
 end
 
+ax.set_yticks([1,2,3,4,5,6,7].-0.5)
+ax.yaxis.set_tick_params(length=0)
+ax.set_yticklabels(distr)
 
-ax.set_xlim(1e-7,1e-3)
-ax.set_ylim(-1,7)
+ax.set_xlim(1e-6,1e-2)
+ax.set_ylim(-1.75,7)
 
 e = abs.(errorsg[1,:])
 p10 = percentile(e,10)
@@ -68,18 +74,19 @@ p50 = percentile(e,50)
 p75 = percentile(e,75)
 p90 = percentile(e,90)
 maxx = maximum(e)
-ax.text(p10,0.1,"10%",rotation=90,ha="center",va="top",fontsize=9)
-ax.text(p25,0.1,"25%",rotation=90,ha="center",va="top",fontsize=9)
-ax.text(p50,0.1,"median",rotation=90,ha="center",va="top",fontsize=9)
-ax.text(p75,0.1,"75%",rotation=90,ha="center",va="top",fontsize=9)
-ax.text(maxx,0.1,"max",rotation=90,ha="center",va="top",fontsize=9)
+ax.text(p10,0.1,"10%",rotation=90,ha="center",va="top",fontsize=10)
+ax.text(p25,0.1,"25%",rotation=90,ha="center",va="top",fontsize=10)
+ax.text(p50,0.1,"median",rotation=90,ha="center",va="top",fontsize=10)
+ax.text(p75,0.1,"75%",rotation=90,ha="center",va="top",fontsize=10)
+ax.text(maxx,0.1,"max",rotation=90,ha="center",va="top",fontsize=10)
 
 ax.plot(0,0,"C0",lw=8,label="Round-to-nearest")
 ax.plot(0,0,"C1",lw=8,label="Bit-grooming")
+ax.plot(0,0,"C2",lw=8,label="Bit-shaving")
 ax.legend(loc=4)
 
-ax.set_yticks([])
+# ax.set_yticks([])
 ax.set_xlabel("absolute mean error")
 # ax.set_ylabel("N")
-ax.set_title("Bias: bit-grooming vs round-to-nearest",loc="left")
+ax.set_title("Bias comparison",loc="left")
 tight_layout()
