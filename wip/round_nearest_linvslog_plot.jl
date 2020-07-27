87e3ce20e03e9fcca99ff688fcb719e8e9c7d4ce
Milan K <milankloewer@gmx.de>
2020-07-27 16:06:56 +0100

    updated plots

--- wip/round_nearest_linvslog_plot.jl
+++ wip/round_nearest_linvslog_plot.jl
@@ -14,9 +14,8 @@ logerror = logerror[:,sortargs,:]
 means = means[sortargs]
 
 ## fit histogram
-ion()
-pygui(true)
-fig,(ax1,ax2,ax3) = subplots(1,3,figsize=(10,10),sharey=true)
+ioff()
+fig,(ax1,ax2,ax3) = subplots(1,3,figsize=(10,8),sharey=true)
 
 lwmm = 1
 lw10 = 2
@@ -62,20 +61,32 @@ ax1.yaxis.set_tick_params(length=0)
 ax2.yaxis.set_tick_params(length=0)
 ax3.yaxis.set_tick_params(length=0)
 ax1.set_yticklabels(allvars)
-ax1.set_ylim(-5,nvars)
+ax1.set_ylim(-6.5,nvars)
 
-ax1.set_xlim(1e-13,1e-1)
-ax2.set_xlim(1e-22,1e1)
-ax3.set_xlim(1e-8,1e1)
+ax1.set_xlim(1e-13,9e-2)
+ax2.set_xlim(2e-22,1e1)
+ax3.set_xlim(2e-8,1e1)
 
-alfa=0.3
+alfa=0.25
 for i in 1:nvars
-    if meanerror[1,i] >= meanerror[3,i]
+    if meanerror[1,i] >= meanerror[3,i] && meanerror[2,i] < meanerror[4,i]
+        ax1.fill_between(ax1.get_xlim(),[i-1,i-1],[i,i],color="C9",alpha=alfa)
+    end
+
+    for (ax,e) in zip([ax2,ax3],[linerror,logerror])
+        if e[1,i,4] >= e[3,i,4] && e[2,i,4] < e[4,i,4]
+            ax.fill_between(ax.get_xlim(),[i-1,i-1],[i,i],color="C9",alpha=alfa)
+        end
+    end
+end
+
+for i in 1:nvars
+    if meanerror[2,i] >= meanerror[4,i]
         ax1.fill_between(ax1.get_xlim(),[i-1,i-1],[i,i],color="C2",alpha=alfa)
     end
 
     for (ax,e) in zip([ax2,ax3],[linerror,logerror])
-        if e[1,i,4] >= e[3,i,4]
+        if e[2,i,4] >= e[4,i,4]
             ax.fill_between(ax.get_xlim(),[i-1,i-1],[i,i],color="C2",alpha=alfa)
         end
     end
@@ -90,9 +101,10 @@ ax1.scatter(0,0,30,"C0",edgecolor="k",label="LinQuant16")
 ax1.scatter(0,0,30,"k",edgecolor="k",label="LinQuant24")
 ax1.scatter(0,0,30,"C1",edgecolor="k",label="LogQuant16")
 ax1.scatter(0,0,30,"C3",edgecolor="k",label="LogQuant24")
-ax3.fill_between(ax3.get_xlim(),[-10,-10],[-9,-9],color="C2",alpha=alfa,label="Log better than LinQuantization\nat 16 bit")
+ax3.fill_between(ax3.get_xlim(),[-10,-10],[-9,-9],color="C9",alpha=alfa,label="at 16 bit")
+ax3.fill_between(ax3.get_xlim(),[-10,-10],[-9,-9],color="C2",alpha=alfa,label="at both 16 and 24 bit")
 ax1.legend(loc=4,ncol=2,fontsize=8,scatterpoints=3)
-ax3.legend(loc=3,fontsize=9)
+ax3.legend(loc=4,fontsize=9,title="Log better than LinQuantization")
 
 ax1.set_xlabel("norm. mean error")
 ax2.set_xlabel("norm. absolute error")
@@ -107,3 +119,5 @@ ax2.set_title("b",loc="right",fontweight="bold")
 ax3.set_title("c",loc="right",fontweight="bold")
 
 tight_layout()
+savefig("/Users/milan/git/Elefridge.jl/plots/linvslog_all.png",dpi=200)
+close(fig)
