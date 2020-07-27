d54a161fd54003cd1a7cb9c0832745c77e23efdc
Milan K <milankloewer@gmx.de>
2020-07-27 16:06:24 +0100

    Faster bitentropy without histogram allocation

--- src/bitentropy.jl
+++ src/bitentropy.jl
@@ -1,25 +1,42 @@
-function bitentropy(n::Integer,A::AbstractArray)
-
-    n == 8*sizeof(eltype(A)) || throw(error("Element type is $(eltype(A)) with
-                            $(sizeof(eltype(A))) bits, incompatible with n=$n"))
-
-    T = whichUInt(n)
-    hist = zeros(UInt32,2^n)
-
-    for a in A
-        hist[reinterpret(T,a)+1] += one(UInt32)
-    end
-
-    # normalize
-    hist /= sum(hist)
-    return entropy(hist,2)
+function bitentropy(A::AbstractArray,base::Real=2)
+    nbits = sizeof(eltype(A))*8
+    T = whichUInt(nbits)
+    nbits == 8 && return bitentropy(T,A,base)
+    nbits == 16 && return bitentropy(T,A,base)
+    nbits == 24 && return bitentropy(T,A,base)
+    nbits == 32 && return bitentropy(T,A,base)
+    nbits == 40 && return bitentropy(T,A,base)
+    nbits == 48 && return bitentropy(T,A,base)
+    nbits == 56 && return bitentropy(T,A,base)
+    nbits == 64 && return bitentropy(T,A,base)
+    throw(error("Only element types with 8,16,24,32,40,48,56,64 bits supported.
+                $nbits-bit $(eltype(A)) provided."))
 end
 
-function bitentropy(A::AbstractArray)
-    nbits = sizeof(eltype(A))*8
-    nbits == 8 && return bitentropy(8,A)
-    nbits == 16 && return bitentropy(16,A)
-    nbits == 24 && return bitentropy(24,A)
-    throw(error("Only element types with 8,16,24 bits supported.
-                $nbits-bit $(eltype(A)) provided."))
+function bitentropy(::Type{T},A::AbstractArray,base::Real=2) where T
+
+    # reinterpret to UInt then sort to avoid allocating a histogram
+    Av = sort(reinterpret.(T,vec(A)))
+    n = length(Av)
+
+    E = 0.0     # entropy
+    m = 1       # counter
+
+    for i in 1:n-1
+        @inbounds if Av[i] == Av[i+1]     # check whether next entry belongs to the same bin in histogram
+            m += 1
+        else
+            p = m/n
+            E -= p*log(p)
+            m = 1
+        end
+    end
+
+    p = m/n         # complete for last bin
+    E -= p*log(p)
+
+    # convert to given base, 2 i.e. [bit] by default
+    E /= log(base)
+
+    return E
 end
