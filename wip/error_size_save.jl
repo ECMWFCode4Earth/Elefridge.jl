c05c893f223388534b822fe7788cf142aef2a920
Milan K <milankloewer@gmx.de>
2020-07-29 18:57:24 +0100

    Include the linlogroundzfp comparison that was overwritten previously

--- wip/error_size_save.jl
+++ wip/error_size_save.jl
@@ -3,152 +3,102 @@ using PyCall
 xr = pyimport("xarray")
 using Blosc
 using Elefridge
-using TranscodingStreams, CodecZlib, CodecZstd, CodecLz4
 using JLD
+using ZfpCompression
 
-DeflateCompressorL9 = DeflateCompressor(level=9,windowbits=15)
-ZstdCompressorL22 = ZstdCompressor(level=22)
+path = "/Users/milan/cams/gridded"
+filelist = filter(x->endswith(x,".grib"),readdir(path))
 
-TranscodingStreams.initialize(DeflateCompressorL9)
-TranscodingStreams.initialize(ZstdCompressorL22)
+n = length(filelist)
+varnames = fill("",n)
+methods = ["Lin24","Log16","R16+Blosc","R14+Blosc","R12+Blosc",
+            "R16+LZ4HC","R14+LZ4HC","R12+LZ4HC",
+            "ZFP15","ZFP12","ZFP9"]
+stats = ["cf","meanerror","absmedian","abs90%","absmax","decmedian","dec90%","decmax"]
 
-path = "/Users/milan/cams"
-allvars = ["no2","go3","so2","aermr04","aermr05","aermr06","ch4","co"]
-# allvars = ["co2","no","clwc","ciwc","t","w"]
+E = fill(0f0,n,length(methods),length(stats))
 
-for vari in [allvars[1]]
-    println("--------")
-    println("Compressing $vari")
+function statscalc(X0::Array{T,N},Xa::Array{T,N}) where {T,N}
+    Xamean = mean(Xa)
+    X0mean = mean(X0)
+    meanerror = abs(X0mean-Xamean)/X0mean       # normalised
 
-    filelist = filter(x->endswith(x,vari*".grib"),readdir(path))
-    X = xr.open_dataarray(joinpath(path,filelist[1]),engine="cfgrib").data
+    abserr = abs.(X0-Xa)/X0mean                 # normalised
+    abserr = sort(vec(abserr))
+    abs50 = T(quantile(abserr,.5,sorted=true))
+    abs90 = T(quantile(abserr,.9,sorted=true))
+    absmax = abserr[end]
 
-    # volume and error norms (columns):
-    # vol, mean, median, 90% and max  of decimal error
-    # mean, median, 90% and max of abs error
-    # total 9
+    # exception cases for decimal error
+    Xa0 = Xa .== zero(T)
+    X00 = X0 .== zero(T)
 
-    # compression methods (rows):
-    # LinQuant24, LogQuant16, RoundNearest16/24 + Blosc/Deflate/Zstd/LZ4
-    # total 10
+    Xas = signbit.(Xa)
+    X0s = signbit.(X0)
 
-    # storing array
-    EV = Array{Float32,2}(undef,10,9)
+    decerr = abs.(log10.(abs.(X0./Xa)))
+
+    decerr[Xas .⊻ X0s] .= Inf        # signs don't match
+    decerr[Xa0 .⊻ X00] .= Inf        # one is zero the other one not
+    decerr[Xa0 .& X00] .= zero(T)    # both zero, set decimal error to zero
+
+    decerr = sort(vec(decerr))
+    dec50 = T(quantile(decerr,.5,sorted=true))
+    dec90 = T(quantile(decerr,.9,sorted=true))
+    decmax = decerr[end]
+
+    return [meanerror,abs50,abs90,absmax,dec50,dec90,decmax]
+end
+
+for (i,file) in enumerate(filelist)
+    varname = split(split(file,"_")[end],".")[1]
+    varnames[i] = varname
+    println("---")
+    println("Reading $varname")
+    X = xr.open_dataarray(joinpath(path,file),engine="cfgrib").data
 
     # transpose array
-    X = copy(transpose(X))
+    X = permutedims(X,[3,2,1])  # unravels as longitude, latitude, level
+
+    negatives = any(X .< 0f0)  # check for negative entries
 
     ## LinQuant24
-    EV[1,1] = 32/24     # compression factor
-    @time Xc = Array{Float32}(LinQuant24Array(X))
-    err = abs.(log10.(X ./ Xc))     # decimal error
-    EV[1,2] = mean(err)
-    EV[1,3] = median(err)
-    EV[1,4] = percentile(vec(err),90)
-    EV[1,5] = maximum(err)
+    E[i,1,1] = 32/24     # compression factor
+    Xc = Array{Float32}(LinQuant24Array(X))
+    E[i,1,2:end] = statscalc(X,Xc)
+    println("LinQuant24 finished.")
 
-    err = abs.(X.-Xc)   # absolute error
-    EV[1,6] = mean(err)
-    EV[1,7] = median(err)
-    EV[1,8] = percentile(vec(err),90)
-    EV[1,9] = maximum(err)
+    ## LogQuant16
+    E[i,2,1] = 32/16
+    if negatives       # don't compress
+        E[i,2,2:end] .= 0
+    else
+        Xc = Array{Float32}(LogQuant16Array(X))
+        E[i,2,2:end] = statscalc(X,Xc)
+    end
+    println("LogQuant16 finished.")
 
-    # LoqQuant16
-    EV[2,1] = 2
-    #Xc = Array{Float32}(LogQuant16Array(X))
-    err = abs.(log10.(X ./ Xc))
-    EV[2,2] = mean(err)
-    EV[2,3] = median(err)
-    EV[2,4] = percentile(vec(err),90)
-    EV[2,5] = maximum(err)
+    ## Round+lossless
+    for (j,r) in enumerate([7,5,3])
+        Xc = round(X,r)         # RN16,14,12
+        E[i,2+j,2:end] = statscalc(X,Xc)
+        E[i,5+j,2:end] = E[i,2+j,2:end]
 
-    err = abs.(X.-Xc)
-    EV[2,6] = mean(err)
-    EV[2,7] = median(err)
-    EV[2,8] = percentile(vec(err),90)
-    EV[2,9] = maximum(err)
+        Blosc.set_compressor("blosclz")
+        E[i,2+j,1] = sizeof(X)/sizeof(compress(Xc,level=5))
+        Blosc.set_compressor("lz4hc")
+        E[i,5+j,1] = sizeof(X)/sizeof(compress(Xc,level=5))
+        println("RN$(r+9)+Blosc/LZ4HC finished.")
+    end
 
-    # RoundNearest16 + Blosc
-    Xc = round(X,7)
-    err = abs.(log10.(X ./ Xc))
-    EV[3,2] = mean(err)
-    EV[3,3] = median(err)
-    EV[3,4] = percentile(vec(err),90)
-    EV[3,5] = maximum(err)
-    Blosc.set_compressor("blosclz")
-    @time Xcc = compress(Xc,level=9)
-    EV[3,1] = sizeof(Xc)/sizeof(Xcc)
-    println("RN16+Blosc: CF=$(EV[3,1])")
+    ## Zfp
+    for (j,r) in enumerate([15,12,9])
+        Xc = zfp_compress(X,precision=r)
+        E[i,8+j,1] = sizeof(X)/sizeof(Xc)
+        X2 = zfp_decompress(Xc)
+        E[i,8+j,2:end] = statscalc(X,X2)
+        println("ZFP$r finished.")
+    end
 
-    err = abs.(X.-Xc)
-    EV[3,6] = mean(err)
-    EV[3,7] = median(err)
-    EV[3,8] = percentile(vec(err),90)
-    EV[3,9] = maximum(err)
-
-    # create a UInt8 view
-    Xc8 = unsafe_wrap(Array, Ptr{UInt8}(pointer(Xc)), sizeof(Xc))
-
-    # RoundNearest16 + LZ4HC
-    EV[4,2:9] = EV[3,2:9]
-    Blosc.set_compressor("lz4hc")
-    @time Xcc = compress(Xc,level=9)
-    EV[4,1] = sizeof(Xc)/sizeof(Xcc)
-    println("RN16+LZ4HC: CF=$(EV[4,1])")
-
-    # RoundNearest16 + Zstd
-    EV[5,2:9] = EV[3,2:9]
-    @time Xcc = transcode(ZstdCompressorL22,Xc8)
-    EV[5,1] = sizeof(Xc)/sizeof(Xcc)
-    println("RN16+Zstd: CF=$(EV[5,1])")
-
-    # RoundNearest16 + Deflate
-    EV[6,2:9] = EV[3,2:9]
-    @time Xcc = transcode(DeflateCompressorL9,Xc8)
-    EV[6,1] = sizeof(Xc)/sizeof(Xcc)
-    println("RN16+Deflate: CF=$(EV[6,1])")
-
-    # # RoundNearest24 + Blosc
-    # Xc = round(X,15)
-    # err = abs.(log10.(X ./ Xc))
-    # EV[7,2] = mean(err)
-    # EV[7,3] = median(err)
-    # EV[7,4] = percentile(vec(err),90)
-    # EV[7,5] = maximum(err)
-    # Blosc.set_compressor("blosclz")
-    # Xcc = compress(Xc,level=9)
-    # EV[7,1] = sizeof(Xc)/sizeof(Xcc)
-    #
-    # err = abs.(X.-Xc)
-    # EV[7,6] = mean(err)
-    # EV[7,7] = median(err)
-    # EV[7,8] = percentile(vec(err),90)
-    # EV[7,9] = maximum(err)
-    #
-    # # create a UInt8 view
-    # Xc8 = unsafe_wrap(Array, Ptr{UInt8}(pointer(Xc)), sizeof(Xc))
-    #
-    # # RoundNearest16 + LZ4
-    # EV[8,2:9] = EV[7,2:9]
-    # Blosc.set_compressor("lz4hc")
-    # Xcc = compress(Xc,level=9)
-    # EV[8,1] = sizeof(Xc)/sizeof(Xcc)
-    #
-    # # RoundNearest16 + Zstd
-    # EV[9,2:9] = EV[7,2:9]
-    # Xcc = transcode(ZstdCompressorL22,Xc8)
-    # EV[9,1] = sizeof(Xc)/sizeof(Xcc)
-    #
-    # # RoundNearest16 + Deflate
-    # EV[10,2:9] = EV[7,2:9]
-    # Xcc = transcode(DeflateCompressorL9,Xc8)
-    # EV[10,1] = sizeof(Xc)/sizeof(Xcc)
-    #
-    # # just lz4hc
-    # Blosc.set_compressor("lz4hc")
-    # Xcc = compress(X,level=9)
-    # lz4hcc = sizeof(X)/sizeof(Xcc)
-    #
-    # save("cams/error/$vari.jld","EV",EV,"lz4",lz4hcc)
-    # println("$vari stored.")
+    @save "/Users/milan/cams/error/linlogroundzfp_all.jld" varnames methods stats E
 end
