7111e52f7f27f238cf2f177348c0efe1f85a5656
milankl <milankloewer@gmx.de>
2021-01-22 02:29:36 +0000

    ensemble compression

--- wip/ensemble_compr.jl
+++ wip/ensemble_compr.jl
@@ -1,9 +1,5 @@
-using PyCall
-using JLD2
-using FileIO
-using NetCDF
-using Elefridge
-using ZfpCompression
+using JLD2, FileIO, NetCDF
+using Elefridge, ZfpCompression
 using TranscodingStreams,CodecZstd
 
 ZstdCompressorL3 = ZstdCompressor(level=3)
@@ -15,61 +11,73 @@ TranscodingStreams.initialize(ZstdCompressorL10)
 ZstdCompressorL22 = ZstdCompressor(level=22)
 TranscodingStreams.initialize(ZstdCompressorL22)
 
-steps = "1-90"
-# steps = "90-144"
-# steps = "150-360"
-path1 = "/data1/ens"
-path2 = "/data2/ens"
-all_files1 = [joinpath(path1,f) for f in filter(x->endswith(x,"steps$steps.ll.nc"),readdir(path1))]
-all_files2 = [joinpath(path2,f) for f in filter(x->endswith(x,"steps$steps.ll.nc"),readdir(path2))]
-
-all_files = vcat(all_files1,all_files2)
-
-# sort members
-all_files_n = sortperm([parse(Int,split(split(f,"member")[2],".steps")[1])
-                        for f in all_files])
-all_files = all_files[all_files_n]
-all_files
+path = "/network/aopp/chaos/pred/kloewer/esowc/"
+member1files = filter(x->endswith(x,".nc"),readdir(joinpath(path,"member1")))
+steps = [parse(Int,split(split(file,"step")[2],".")[1]) for file in member1files]
+sort!(steps)
 
 # parameter
-Nens = length(all_files)
-Ntsteps = [90,18,36][steps .== ["1-90","90-144","150-360"]][1]
+Nens = 25
+Nbits = 32
 Nlon = 1800
-Nlat = 451
-Nvert = 11
-every = 5
+Nlat = 901
+Nvert = 91
+Ntsteps = length(steps)
+chunk_size = 8
+nchunks = Nvert ÷ chunk_size
+keepbits = [8,7,6,5]
 
-# preallocate bitwise information content
-Xzfp,Xzstd = try load("/home/milan/analysis/ensemble_compression_$(steps).jld2","Xzfp","Xzstd") catch; zeros(Float64,Ntsteps),zeros(Float64,Ntsteps) end
+# preallocate
+Xzfp,Xzstd = try load(joinpath(path,"analysis/ensemble_compression_$(Nens)members_s5-8.jld2"),"Xzfp","Xzstd") catch; zeros(Float64,Ntsteps,length(keepbits)),zeros(Float64,Ntsteps,length(keepbits)) end
     
-for t in 77:Ntsteps
-    println("Time step $t")
+for t in 13:Ntsteps
+    println("Time step $(steps[t])h")
     temp = fill(0f0,Nens,Nlon,Nlat,Nvert)
    
-    print("Ensemble member ")
+    print("Loading ensemble member ")
     # read the data
     for ie in 1:Nens
         print("$ie,")
-        ncfile = NetCDF.open(all_files[ie])
-        temp[ie,:,:,:] = ncfile.vars["t"][:,451:end,:,t]
+        ncfile = NetCDF.open(joinpath(path,"member$ie","ensemble.t.member$ie.step$(steps[t]).ll.nc"))
+        temp[ie,:,:,:] = ncfile.vars["t"][1:Nlon,1:Nlat,1:Nvert]
     end
-    println("All loaded.")
+    println("\nCompressing...")
     
-#     precision = t<5 ? 14 : 13
-#     keepbits = t<5 ? 7 : 6
-    precision = 14
-    keepbits = 7
+    # ZFP COMPRESSION
+    for (ik,keepbit) in enumerate(keepbits)
+        
+        precision = keepbit+7   # For 4D compression
     
-    # compress
-    Xzfp[t] = 2*sizeof(temp)/sizeof(zfp_compress(temp,precision=precision))
-    println("CF ZFP: $(Xzfp[t])")
-
-    round!(temp,keepbits)
-    Xr8 = copy(reinterpret(UInt8,vec(temp)))
-    Xzstd[t] = 2*sizeof(Xr8)/sizeof(transcode(ZstdCompressorL10,Xr8))
-    println("CF ZSTD: $(Xzstd[t])")
+        # ZFP: compress in chunks
+        compressed_size = 0
+    
+        for ichunk in 1:nchunks-1    # compress the last one after loop
+            print("$ichunk,")
+            start = (ichunk-1)*chunk_size+1
+            stop = start+chunk_size-1
+            temp_chunk = temp[:,:,:,start:stop]
+            compressed_size += sizeof(zfp_compress(temp_chunk,precision=precision))
+        end
+    
+        # compress last chunk
+        start = (nchunks-1)*chunk_size+1
+        temp_chunk = temp[:,:,:,start:end]
+        compressed_size += sizeof(zfp_compress(temp_chunk,precision=precision))
+    
+        Xzfp[t,ik] = 2*sizeof(temp)/compressed_size
+        println("$(keepbit)bits, CF ZFP: $(Xzfp[t,ik])")
+        save(joinpath(path,"analysis/ensemble_compression_$(Nens)members_s5-8.jld2"),"Xzfp",Xzfp,"Xzstd",Xzstd)
+    end
+    
+    # ZSTD COMPRESSION
+    for (ik,keepbit) in enumerate(keepbits)
+        round!(temp,keepbit)
+        Xr8 = copy(reinterpret(UInt8,vec(temp)))
+        Xzstd[t,ik] = 2*sizeof(Xr8)/sizeof(transcode(ZstdCompressorL10,Xr8))
+        println("$(keepbit)bits, CF ZSTD: $(Xzstd[t,ik])")
+        save(joinpath(path,"analysis/ensemble_compression_$(Nens)members_s5-8.jld2"),"Xzfp",Xzfp,"Xzstd",Xzstd)
+    end
     
     println("Compression done.")
-    save("/home/milan/analysis/ensemble_compression_$(steps).jld2","Xzfp",Xzfp,"Xzstd",Xzstd)
     println("---")
 end
